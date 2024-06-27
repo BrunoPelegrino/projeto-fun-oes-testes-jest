963264f6e1b6992c7af6123a54d8d9253e3483fb
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2024-06-27 18:13:54 -0300

    implemente funçoes getAnimalsOlderThan & getEmployeeByName

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
+const { species } = data
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const findSpecie = species.find((animals) => animals.name === animal);
+  const verifyAge = findSpecie.residents.every((resident) => resident.age >= age);
+  return verifyAge
+  
 }
+
+console.log(getAnimalsOlderThan('lions', 5));
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
+const { employees } = data;
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const findEmployee = employees.find(
+    (emplyee) =>
+      emplyee.firstName === employeeName || emplyee.lastName === employeeName
+  );
+  return findEmployee
 }
+
+console.log(getEmployeeByName('Nelson'));
 
 module.exports = getEmployeeByName;
