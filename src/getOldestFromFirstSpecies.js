5238ca8c03e1f27d4f494c9cca5351559cd3bcef
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2024-06-28 14:23:04 -0300

    implementa funçao getOldestFromFirsSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
+const { employees, species } = data
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const findPerson = employees.find((findById) => findById.id === id);
+  const responsibleFor = findPerson.responsibleFor[0];
+  const findSpecieById = species.find((specie) => specie.id === responsibleFor).residents;
+
+  
+  const teste = findSpecieById.reduce((acc, oldest) =>  acc.age > oldest.age ?  acc : oldest, 0)
+  
+
+  return [teste.name, teste.sex, teste.age]
 }
 
 module.exports = getOldestFromFirstSpecies;
